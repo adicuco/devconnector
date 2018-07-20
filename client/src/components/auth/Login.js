d13a7a13c50b16ed854eda9edae5156db415109a
Adi Cucolaș <cucolas.adi@gmail.com>
2018-07-20 13:39:30 +0200

    added Register & Login forms with state

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,12 +1,68 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      errors: {}
+    };
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    console.log(user);
+  };
+
   render() {
+    const { email, password } = this.state;
+    const { onChange, onSubmit } = this;
+
     return (
-      <div>
-        <h1>Login</h1>
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">Sign in to your DevConnector account</p>
+              <form onSubmit={onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={email}
+                    onChange={onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={password}
+                    onChange={onChange}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 export default Login;
