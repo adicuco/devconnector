84e2087387ce4252d6845a3bdc261e43a0a20f85
Adi Cucolaș <cucolas.adi@gmail.com>
2018-07-20 15:36:48 +0200

    Register frontend errors

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import classnames from 'classnames';
 
 class Register extends Component {
   constructor() {
@@ -25,11 +27,15 @@ class Register extends Component {
       password: this.state.password,
       password2: this.state.password2
     };
-    console.log(newUser);
+
+    axios
+      .post('/api/users/register', newUser)
+      .then(res => console.log(res.data))
+      .catch(err => this.setState({ errors: err.response.data }));
   };
 
   render() {
-    const { name, email, password, password2 } = this.state;
+    const { name, email, password, password2, errors } = this.state;
     const { onChange, onSubmit } = this;
 
     return (
@@ -43,22 +49,28 @@ class Register extends Component {
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.name
+                    })}
                     placeholder="Name"
                     name="name"
                     value={name}
                     onChange={onChange}
                   />
+                  <div className="invalid-feedback">{errors.name}</div>
                 </div>
                 <div className="form-group">
                   <input
                     type="email"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.email
+                    })}
                     placeholder="Email Address"
                     name="email"
                     value={email}
                     onChange={onChange}
                   />
+                  <div className="invalid-feedback">{errors.email}</div>
                   <small className="form-text text-muted">
                     This site uses Gravatar so if you want a profile image, use a Gravatar email
                   </small>
@@ -66,22 +78,28 @@ class Register extends Component {
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password
+                    })}
                     placeholder="Password"
                     name="password"
                     value={password}
                     onChange={onChange}
                   />
+                  <div className="invalid-feedback">{errors.password}</div>
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password2
+                    })}
                     placeholder="Confirm Password"
                     name="password2"
                     value={password2}
                     onChange={onChange}
                   />
+                  <div className="invalid-feedback">{errors.password2}</div>
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
