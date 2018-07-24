a62cecbe966f6264b5749d6a68d1d2256b71faf4
Adi Cucolaș <cucolas.adi@gmail.com>
2018-07-24 13:51:51 +0200

    added Dashboard Component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -11,6 +12,7 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 
 import './App.css';
 
@@ -26,9 +28,10 @@ if (localStorage.jwtToken) {
   // Check for expired token
   const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
+    // Clear current Profile
+    store.dispatch(clearCurrentProfile());
     //Logout user
     store.dispatch(logoutUser());
-    // TODO: Clear current Profile
     // Redirect to login
     window.location.href = '/login';
   }
@@ -45,6 +48,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
---
+++ client/src/components/dashboard/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="py-3 text-center">
+      <i className="fas fa-cog fa-spin fa-2x" />
+    </div>
+  );
+};
