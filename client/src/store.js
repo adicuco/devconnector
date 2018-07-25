5469d3df5f99321fc0f06e2cb00cc6403a98939e
Adi Cucolaș <cucolas.adi@gmail.com>
2018-07-25 23:04:59 +0200

    Fixed store.js to use Redux extension just for dev

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,18 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+let store;
+if (process.env.NODE_ENV === 'production') {
+  store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware)));
+} else {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+}
 
 export default store;
