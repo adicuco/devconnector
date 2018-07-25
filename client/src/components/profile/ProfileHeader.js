c229301a9062b729956a8f45825c838fb204d71a
Adi Cucolaș <cucolas.adi@gmail.com>
2018-07-25 12:23:16 +0200

    Update ProfileHeader

--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -18,7 +18,7 @@ class ProfileHeader extends Component {
               <h1 className="display-4 text-center">{profile.user.name}</h1>
               <p className="lead text-center">
                 {profile.status}{' '}
-                {isEmpty(profile.company) ? null : <span>at {profile.company}</span>}
+                {isEmpty(profile.company) ? null : <span>@ {profile.company}</span>}
               </p>
               {isEmpty(profile.location) ? null : <p>{profile.location}</p>}
               <p>
@@ -27,19 +27,36 @@ class ProfileHeader extends Component {
                     <i className="fas fa-globe fa-2x" />
                   </a>
                 )}
-
-                <a className="text-white p-2" href="#">
-                  <i className="fab fa-twitter fa-2x" />
-                </a>
-                <a className="text-white p-2" href="#">
-                  <i className="fab fa-facebook fa-2x" />
-                </a>
-                <a className="text-white p-2" href="#">
-                  <i className="fab fa-linkedin fa-2x" />
-                </a>
-                <a className="text-white p-2" href="#">
-                  <i className="fab fa-instagram fa-2x" />
-                </a>
+                {isEmpty(profile.social && profile.social.twitter) ? null : (
+                  <a className="text-white p-2" href={profile.social.twitter} target="_blank">
+                    <i className="fab fa-twitter fa-2x" />
+                  </a>
+                )}
+                {isEmpty(profile.social && profile.social.facebook) ? null : (
+                  <a className="text-white p-2" href={profile.social.facebook} target="_blank">
+                    <i className="fab fa-facebook fa-2x" />
+                  </a>
+                )}
+                {isEmpty(profile.social && profile.social.linkedin) ? null : (
+                  <a className="text-white p-2" href={profile.social.linkedin} target="_blank">
+                    <i className="fab fa-linkedin fa-2x" />
+                  </a>
+                )}
+                {isEmpty(profile.social && profile.social.instagram) ? null : (
+                  <a className="text-white p-2" href={profile.social.instagram} target="_blank">
+                    <i className="fab fa-instagram fa-2x" />
+                  </a>
+                )}
+                {isEmpty(profile.social && profile.social.youtube) ? null : (
+                  <a className="text-white p-2" href={profile.social.youtube} target="_blank">
+                    <i className="fab fa-youtube fa-2x" />
+                  </a>
+                )}
+                {isEmpty(profile.social && profile.social.github) ? null : (
+                  <a className="text-white p-2" href={profile.social.github} target="_blank">
+                    <i className="fab fa-github fa-2x" />
+                  </a>
+                )}
               </p>
             </div>
           </div>
--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -16,7 +16,7 @@ class ProfileItem extends Component {
           <div className="col-lg-6 col-md-4 col-8">
             <h3>{profile.user.name}</h3>
             <p>
-              {profile.status} {isEmpty(profile.company) ? null : <span>at {profile.company}</span>}
+              {profile.status} {isEmpty(profile.company) ? null : <span>@ {profile.company}</span>}
             </p>
             <p>{isEmpty(profile.location) ? null : <span>{profile.location}</span>}</p>
             <Link to={`/profile/${profile.handle}`} className="btn btn-info">
