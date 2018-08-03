5e5d0b49e6689c8ca4a33af87b7348577862e4a0
Adi Cucolaș <cucolas.adi@gmail.com>
2018-08-03 16:01:59 +0200

    Refactor store

--- client/src/store.js
+++ client/src/store.js
@@ -6,18 +6,13 @@ const initialState = {};
 
 const middleware = [thunk];
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 let store;
 if (process.env.NODE_ENV === 'production') {
   store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware)));
 } else {
-  store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-      applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
+  store = createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(...middleware)));
 }
 
 export default store;
