1bbc5dd7016f7b025d59f5f85b7f64b05bfe392a
Adi Cucolaș <cucolas.adi@gmail.com>
2018-08-06 14:48:17 +0200

    Refactor promises into async

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -5,39 +5,36 @@ import jwt_decode from 'jwt-decode';
 import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // Register User
-export const registerUser = (userData, history) => dispatch => {
-  axios
-    .post('/api/users/register', userData)
-    .then(res => history.push('/login'))
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+export const registerUser = (userData, history) => async dispatch => {
+  try {
+    await axios.post('/api/users/register', userData);
+    history.push('/login');
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
+  }
 };
 
 // Login - Get User Token
-export const loginUser = userData => dispatch => {
-  axios
-    .post('/api/users/login', userData)
-    .then(res => {
-      // Save token to localStorage
-      const { token } = res.data;
-      localStorage.setItem('jwtToken', token);
-      // Set token to Auth header
-      setAuthToken(token);
-      // Decode token to get user data
-      const decoded = jwt_decode(token);
-      // Set current user
-      dispatch(setCurrentUser(decoded));
-    })
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+export const loginUser = userData => async dispatch => {
+  try {
+    const res = await axios.post('/api/users/login', userData);
+    const { token } = res.data;
+    localStorage.setItem('jwtToken', token);
+    // Set token to Auth header
+    setAuthToken(token);
+    // Decode token to get user data
+    const decoded = jwt_decode(token);
+    // Set current user
+    dispatch(setCurrentUser(decoded));
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
+  }
 };
 
 // Log user out
