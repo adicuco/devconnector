81a5276188da1b1913de49d46bc076c5d85deb73
Adi Cucolaș <cucolas.adi@gmail.com>
2018-08-06 15:11:24 +0200

    Refactor promises into async

--- client/src/redux/actions/postActions.js
+++ client/src/redux/actions/postActions.js
@@ -11,91 +11,97 @@ import {
 } from './types';
 
 // Add Post
-export const addPost = postData => dispatch => {
-  axios
-    .post('/api/posts', postData)
-    .then(res => dispatch({ type: ADD_POST, payload: res.data }))
-    .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+export const addPost = postData => async dispatch => {
+  try {
+    const res = await axios.post('/api/posts', postData);
+    dispatch({ type: ADD_POST, payload: res.data });
+  } catch (err) {
+    dispatch({ type: GET_ERRORS, payload: err.response.data });
+  }
 };
 
 // Get Posts
-export const getPosts = () => dispatch => {
-  dispatch(setPostLoading());
-  axios
-    .get('/api/posts')
-    .then(res => dispatch({ type: GET_POSTS, payload: res.data }))
-    .catch(err => dispatch({ type: GET_POSTS, payload: null }));
+export const getPosts = () => async dispatch => {
+  try {
+    dispatch(setPostLoading());
+    const res = await axios.get('/api/posts');
+    dispatch({ type: GET_POSTS, payload: res.data });
+  } catch (err) {
+    dispatch({ type: GET_POSTS, payload: null });
+  }
 };
 
 // Get Post by ID
-export const getPost = id => dispatch => {
-  dispatch(setPostLoading());
-  axios
-    .get(`/api/posts/${id}`)
-    .then(res => dispatch({ type: GET_POST, payload: res.data }))
-    .catch(err => dispatch({ type: GET_POST, payload: null }));
+export const getPost = id => async dispatch => {
+  try {
+    dispatch(setPostLoading());
+    const res = await axios.get(`/api/posts/${id}`);
+    dispatch({ type: GET_POST, payload: res.data });
+  } catch (err) {
+    dispatch({ type: GET_POST, payload: null });
+  }
 };
 
 // Delete Post
-export const deletePost = id => dispatch => {
-  axios
-    .delete(`/api/posts/${id}`)
-    .then(res => dispatch({ type: DELETE_POST, payload: id }))
-    .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+export const deletePost = id => async dispatch => {
+  try {
+    await axios.delete(`/api/posts/${id}`);
+    dispatch({ type: DELETE_POST, payload: id });
+  } catch (err) {
+    dispatch({ type: GET_ERRORS, payload: err.response.data });
+  }
 };
 
 // Add Comment
-export const addComment = (postId, commentData) => dispatch => {
-  axios
-    .post(`/api/posts/comment/${postId}`, commentData)
-    .then(res => dispatch({ type: GET_POST, payload: res.data }))
-    .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+export const addComment = (postId, commentData) => async dispatch => {
+  try {
+    const res = await axios.post(`/api/posts/comment/${postId}`, commentData);
+    dispatch({ type: GET_POST, payload: res.data });
+  } catch (err) {
+    dispatch({ type: GET_ERRORS, payload: err.response.data });
+  }
 };
 
 // Delete Comment
-export const deleteComment = (postId, commentId) => dispatch => {
-  axios
-    .delete(`/api/posts/comment/${postId}/${commentId}`)
-    .then(res => dispatch({ type: GET_POST, payload: res.data }))
-    .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+export const deleteComment = (postId, commentId) => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+    dispatch({ type: GET_POST, payload: res.data });
+  } catch (err) {
+    dispatch({ type: GET_ERRORS, payload: err.response.data });
+  }
 };
 
 // Like/Unlike Post
-export const likePost = id => dispatch => {
-  axios
-    .post(`/api/posts/like/${id}`)
-    .then(({ data }) => {
-      dispatch({
-        type: LIKE_POST,
-        payload: data
-      });
-    })
-    .catch(({ response }) => {
-      dispatch({
-        type: GET_ERRORS,
-        payload: response.data
-      });
+export const likePost = id => async dispatch => {
+  try {
+    const res = await axios.post(`/api/posts/like/${id}`);
+    dispatch({
+      type: LIKE_POST,
+      payload: res.data
     });
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
+  }
 };
 
 // Like/Unlike Comment
-export const likeComment = (postId, commentId) => dispatch => {
-  axios
-    .post(`/api/posts/comment/like/${postId}/${commentId}`)
-    .then(({ data }) => {
-      dispatch({
-        type: GET_POST,
-        payload: data
-      });
-    })
-    .catch(({ response }) => {
-      console.log(response);
-
-      dispatch({
-        type: GET_ERRORS,
-        payload: response.data
-      });
+export const likeComment = (postId, commentId) => async dispatch => {
+  try {
+    const res = await axios.post(`/api/posts/comment/like/${postId}/${commentId}`);
+    dispatch({
+      type: GET_POST,
+      payload: res.data
     });
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
+  }
 };
 
 // Set Post loading
